fddd08b1a68c65c2786b39af86a6e7c38e062c69
Amr Gaber <amrtgaber@gmail.com>
2026-01-25 14:42:57 -0600

    update commit hook to fail properly

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider, createRouter } from "@tanstack/react-router"
-import { routeTree } from "./routeTree.gen"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 import "./index.css"
+import { routeTree } from "./routeTree.gen"
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      retry: 1,
       staleTime: 1000 * 60 * 5,
       refetchOnWindowFocus: false,
     },
